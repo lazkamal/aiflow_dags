c0d84c1786700eb4e794fe9d4a868e05fa6d26da
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-22 23:24:25 +0100

    Update dag_test.py

--- dag_test.py
+++ dag_test.py
@@ -5,7 +5,7 @@ from airflow.operators.python_operator import PythonOperator
 
 dag = DAG(dag_id="dag_test", start_date=airflow.utils.dates.days_ago(3), schedule_interval="@daily")
 def test_bash_operator():
-    test = BashOperator(task_id="test", bash_command="echo testme", xcom_push=True, dag= dag)
+    test = BashOperator(task_id="test", bash_command="echo testme", dag=dag)
     result = test.execute(context={})
     assert result == "testme"
     
