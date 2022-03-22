b4a9cd9b44b69bbe810dd490e09960a18dfa113e
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-22 23:10:00 +0100

    Add files via upload

---
+++ dag_test.py
@@ -0,0 +1,12 @@
+import airflow.utils.dates
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+dag = DAG(dag_id="dag_test", start_date=airflow.utils.dates.days_ago(3), schedule_interval="@daily")
+def test_bash_operator():
+    test = BashOperator(task_id="test", bash_command="echo testme", xcom_push=True)
+    result = test.execute(context={})
+    assert result == "testme"
+
+
