f5e8d4b496fdbd3f0b41677fd9472baab7ed6dee
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-23 11:04:19 +0100

    Update dag_test_3.py

--- dag_test_3.py
+++ dag_test_3.py
@@ -7,6 +7,10 @@ from airflow.operators.python import PythonOperator
 def test_dagbag():
     dag_bag = DagBag()
     assert not dag_bag.import_errors
+    
+    for dag_id, dag in dag_bag.dags.items():
+        error_msg= f"{dag_id} in {dag.full_filepath} has no tags"
+        assert dag.tags, error_msg
 
 with DAG(
     dag_id="dag_test_3",
