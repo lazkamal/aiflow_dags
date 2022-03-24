b3df36981bb9583d773e5993682bf6ae1c3f68a7
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-25 00:24:00 +0100

    Update dag_test_3.py

--- dag_test_3.py
+++ dag_test_3.py
@@ -2,17 +2,16 @@ from airflow.models import DagBag
 from airflow.utils.dates import days_ago
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
-
-try:
-    def test_dagbag():
+def test_dagbag():
         dag_bag = DagBag(include_examples=False)
         assert not dag_bag.import_errors
         for dag_id, dag in dag_bag.dags.items():
-            msg = f"{dag_id} in {dag.full_filepath} has a description"
+            m = f"{dag_id} in {dag.full_filepath} has a description"
             error_msg = f"{dag_id} in {dag.full_filepath} has no description"
             assert dag.description, error_msg
-            print(msg)
-
+            print(m)
+try:
+    
     with DAG(
             dag_id="dag_test_3",
             schedule_interval="@daily",
