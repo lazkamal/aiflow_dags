33c7856de6297e72e0871ee9fbb409cac0e4721b
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-24 23:56:12 +0100

    Add files via upload

---
+++ dag_test_3.py
@@ -0,0 +1,27 @@
+from airflow.models import DagBag
+from airflow.utils.dates import days_ago
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+
+try:
+    def test_dagbag():
+        dag_bag = DagBag(include_example=False)
+        assert not dag_bag.import_errors
+        for dag_id, dag in dag_bag.dags.items():
+            error_msg = f"{dag_id} in {dag.full_filepath} has no description"
+            assert dag.description, error_msg
+
+    with DAG(
+            dag_id="dag_test_3",
+            schedule_interval="@daily",
+            start_date=days_ago(2),
+    ) as dag:
+        run_this = PythonOperator(
+            task_id="test",
+            python_callable=test_dagbag,
+        )
+
+# the errror_message provided by the user gets printed
+except AssertionError as msg:
+    print(msg)
+
