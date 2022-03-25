ca87e5e830582d5985f92e38f0ee638a0c44941d
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-25 11:30:42 +0100

    Update dag_test_3.py

--- dag_test_3.py
+++ dag_test_3.py
@@ -6,10 +6,10 @@ from airflow.operators.python import PythonOperator
 def test_dagbag():
     dag_bag = DagBag(include_examples=False)
     for dag_id, dag in dag_bag.dags.items():
-	    m = f"{dag_id} in {dag.full_filepath} has description"
-            error_msg = f"{dag_id} in {dag.full_filepath} has no description"
-            assert dag.description, "Invalid"
-	    print(m)
+	m = f"{dag_id} in {dag.full_filepath} has description"
+        error_msg = f"{dag_id} in {dag.full_filepath} has no description"
+        assert dag.description, "Invalid"
+	print(m)
 try:
     
     with DAG(
