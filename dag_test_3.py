e9f0402e39f3ed63d4a7b1c885e209d5b003c1ab
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-25 12:00:59 +0100

    Update dag_test_3.py

--- dag_test_3.py
+++ dag_test_3.py
@@ -8,9 +8,7 @@ def test_dagbag():
         assert not dag_bag.import_errors
         for dag_id, dag in dag_bag.dags.items():
             error_msg = f"{dag_id} in {dag.full_filepath} has no description"
-	    m = f"{dag_id} in {dag.full_filepath} has description"
             assert dag.description, "Invalid"
-	    print(m)
 try:
 	
 
