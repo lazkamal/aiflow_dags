718721367da3acaf0963eb64c456723a344b019b
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-25 11:39:48 +0100

    Update dag_test_3.py

--- dag_test_3.py
+++ dag_test_3.py
@@ -3,24 +3,26 @@ from airflow.utils.dates import days_ago
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 
-def test_dagbag():
-    dag_bag = DagBag(include_examples=False)
-    for dag_id, dag in dag_bag.dags.items():
-	m = f"{dag_id} in {dag.full_filepath} has description"
-        error_msg = f"{dag_id} in {dag.full_filepath} has no description"
-        assert dag.description, "Invalid"
-	print(m)
 try:
-    
+    def test_dagbag():
+        dag_bag = DagBag(include_example=False)
+        assert not dag_bag.import_errors
+        for dag_id, dag in dag_bag.dags.items():
+            error_msg = f"{dag_id} in {dag.full_filepath} has no description"
+	    m = f"{dag_id} in {dag.full_filepath} has no description"
+            assert dag.description, error_msg
+	    print(m)
+
     with DAG(
-            dag_id="dag_test_3",
-            schedule_interval="@daily",
-            start_date=days_ago(2),
+        dag_id="dag_test_3",
+        schedule_interval="@daily",
+        start_date=days_ago(2),
     ) as dag:
         run_this = PythonOperator(
             task_id="test",
             python_callable=test_dagbag,
         )
+
 # the errror_message provided by the user gets printed
 except AssertionError as msg:
     print(msg)
