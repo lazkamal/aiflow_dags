0cada1d9926c6eb4083238a86c6fd5ccd9549f8a
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-26 14:24:16 +0100

    Add files via upload

---
+++ dag_test_5.py
@@ -0,0 +1,31 @@
+from airflow.models import DagBag
+from airflow.utils.dates import days_ago
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+
+try:
+    def test_dagbag():
+        dag_bag = DagBag(include_examples=False)
+        assert not dag_bag.import_errors
+        for dag_id, dag in self.dagbag.dags.iteritems():
+            emails = dag.default_args.get('email', [])
+            msg = 'Alert email not set for DAG {id}'.format(id=dag_id)
+            self.assertIn('lazkamal34@gmail.com', emails, msg)
+
+
+    with DAG(
+        dag_id="dag_test_5",
+        schedule_interval="@daily",
+        start_date=days_ago(2),
+    ) as dag:
+        run_this = PythonOperator(
+            task_id="test",
+            python_callable=test_dagbag,
+        )
+
+# the errror_message provided by the user gets printed
+except AssertionError as msg:
+    print(msg)
+
+
+
