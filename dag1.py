2eb13bb12cf5207f02e8a1e4b7562e139139db85
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-26 15:10:02 +0100

    Update dag1.py

--- dag1.py
+++ dag1.py
@@ -34,7 +34,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': airflow.utils.dates.days_ago(2),
-    'email': ['airflow@example.com'],
+    'email': ['kamal.lazhar@external.stellantis.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
