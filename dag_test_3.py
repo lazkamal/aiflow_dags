78d850f6d6bf199f8a57b00d03514f1b9aba4810
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-26 11:19:58 +0100

    Update dag_test_3.py

--- dag_test_3.py
+++ dag_test_3.py
@@ -5,6 +5,7 @@ from airflow.operators.python import PythonOperator
 
 def test_dagbag():
         dag_bag = DagBag(include_examples=False)
+	print(dag_bag.dags.items())
         # assert not dag_bag.import_errors
         for dag_id, dag in dag_bag.dags.items():
             error_msg = f"{dag_id} in {dag.full_filepath} has no description"
