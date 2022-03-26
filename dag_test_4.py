8150efd7a651455b9ecaba824f526667c91b5bba
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-26 14:25:57 +0100

    Update dag_test_4.py

--- dag_test_4.py
+++ dag_test_4.py
@@ -9,8 +9,9 @@ try:
         assert not dag_bag.import_errors
         for dag_id, dag in dag_bag.dags.items():
             error_msg = f"{dag_id} in {dag.full_filepath} has no description"
+            valid_msg = f"{dag_id} in {dag.full_filepath} has description"
             if dag.description:
-                print("Valid Dag")
+                print(valid_msg)
             else:
                 print(error_msg)
 
