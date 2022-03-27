c52e507baf611879d08dcd9b70de08080418edea
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-03-27 23:48:11 +0200

    Update dag1.py

--- dag1.py
+++ dag1.py
@@ -57,7 +57,7 @@ default_args = {
 dag = DAG(
     'tutorial',
     default_args=default_args,
-    #description='A simple tutorial DAG',
+    description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
 )
 
