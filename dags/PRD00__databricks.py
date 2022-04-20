1cfe2d84faf36eb86f227de8efc676e4d0725b25
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-04-20 13:20:53 +0200

    Update PRD00__databricks.py

--- dags/PRD00__databricks.py
+++ dags/PRD00__databricks.py
@@ -8,7 +8,7 @@ DAG_ID = os.path.basename(__file__).replace(".py", "")
 DEFAULT_ARGS = {
     "owner": "airflow",
     "depends_on_past": False,
-    "retries": 1,
+    "retries": 3,
     "email_on_failure": False,
     "email_on_retry": False,
 }
