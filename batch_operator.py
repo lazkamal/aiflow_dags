96470a08bd0e87f72f885317fc87ab91cfaddd71
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-05-11 09:55:42 +0200

    Add files via upload

---
+++ batch_operator.py
@@ -0,0 +1,29 @@
+from airflow.providers.microsoft.azure.operators.batch import AzureBatchOperator
+from datetime import datetime
+
+from airflow import DAG
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'start_date': datetime.now() - timedelta(minutes=20),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+dag = DAG(dag_id='batch_operator',
+          default_args=default_args,
+          schedule_interval='@daily',
+          dagrun_timeout=timedelta(seconds=120))
+t1_bash = """
+echo 'Hello World'
+"""
+t1 = AzureBatchOperator(
+    batch_pool_id='airflow-test-pool',
+    batch_pool_vm_size='standard_a2_v2',
+    batch_job_id='myJob_test',
+    batch_task_command_line="echo 'Hello World'",
+    batch_task_id='myTask',
+    dag=dag)
