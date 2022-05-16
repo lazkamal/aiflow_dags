e34f4084feda46548d9ef048889d833283e4854d
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-05-16 15:11:06 +0200

    Update PRD00__databricks.py

--- dags/PRD00__databricks.py
+++ dags/PRD00__databricks.py
@@ -1,40 +1,50 @@
-import os
+
+"""
+This is an example DAG which uses the DatabricksSubmitRunOperator.
+In this example, we create two tasks which execute sequentially.
+The first task is to run a notebook at the workspace path "/test"
+and the second task is to run a JAR uploaded to DBFS. Both,
+tasks use new clusters.
+
+Because we have set a downstream dependency on the notebook task,
+the spark jar task will NOT run until the notebook task completes
+successfully.
+
+The definition of a successful run is if the run has a result_state of "SUCCESS".
+For more information about the state of a run refer to
+https://docs.databricks.com/api/latest/jobs.html#runstate
+"""
 
 from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
-DAG_ID = os.path.basename(__file__).replace(".py", "")
-DEFAULT_ARGS = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "retries": 2,
-    "email_on_failure": False,
-    "email_on_retry": False,
-}
 
 with DAG(
-    dag_id=DAG_ID,
-    description="In this example, we create two tasks which execute sequentially. The first task is to run a notebook at the workspace path '/test' and the second task is to run a JAR uploaded to DBFS",
+    dag_id='example_databricks_operator',
     schedule_interval='@daily',
-    default_args=DEFAULT_ARGS,
     start_date=datetime(2021, 1, 1),
-    tags=['databricks Submit Dag'],
+    tags=['example'],
     catchup=False,
 ) as dag:
     # [START howto_operator_databricks_json]
     # Example of using the JSON parameter to initialize the operator.
     new_cluster = {
         'spark_version': '9.1.x-scala2.12',
-        'node_type_id': 'r3.xlarge',
-        'aws_attributes': {'availability': 'ON_DEMAND'},
-        'num_workers': 8,
+        'node_type_id': 'Standard_DS3_v2',
+        'num_workers': 2,
     }
 
     notebook_task_params = {
         'new_cluster': new_cluster,
         'notebook_task': {
-            'notebook_path': '/Users/airflow@example.com/PrepareData',
+            'notebook_path': '/Users/sc87291@inetpsa.com/test1',
+            "email_notifications": {
+                
+                "on_start": [ "kamal.lazhar@external.stellantis.com" ],
+                "on_success": [ "kamal.lazhar@external.stellantis.com" ],
+                "on_failure": []   
+            }
         },
     }
 
@@ -47,8 +57,11 @@ with DAG(
     spark_jar_task = DatabricksSubmitRunOperator(
         task_id='spark_jar_task',
         new_cluster=new_cluster,
-        spark_jar_task={'main_class_name': 'com.example.ProcessData'},
-        libraries=[{'jar': 'dbfs:/lib/etl-0.1.jar'}],
+        spark_jar_task={'main_class_name': 'org.apache.spark.examples.SparkPi',
+                        'parameters': '10'
+                       },
+        libraries=[{'jar': 'dbfs:/FileStore/sparkpi/sparkpi_assembly_0_1.jar'}],
     )
+    
     # [END howto_operator_databricks_named]
     notebook_task >> spark_jar_task
