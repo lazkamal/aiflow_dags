d5c752b84899575a32ab4a874e3831329c66f730
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-05-18 14:43:54 +0200

    Create Update_DatabricksRepo.py

---
+++ Update_DatabricksRepo.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks_repos import DatabricksReposCreateOperator
+
+with DAG(
+        dag_id='Update_DatabricksRepo',
+        schedule_interval='@daily',
+        start_date=datetime.now(),
+        tags=['example'],
+        catchup=False,
+) as dag:
+    repo_path = "/Repos/sc87291@inetpsa.com/airflow-repo"
+    git_url = "https://github.com/lazkamal/dags_2.git"
+    create_repo = DatabricksReposUpdateOperator(task_id='update_repo', repo_path=repo_path, branch='main', databricks_conn_id='databricks_default')
