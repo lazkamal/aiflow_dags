58d3388f434486a193674023ec72566d33ebd067
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-05-18 09:21:21 +0200

    Add files via upload

---
+++ Create_DatabricksRepo.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks_repos import DatabricksReposCreateOperator
+
+with DAG(
+        dag_id='example_databricks_operator',
+        schedule_interval='@daily',
+        start_date=datetime(2021, 1, 1),
+        tags=['example'],
+        catchup=False,
+) as dag:
+    repo_path = "/Repos/sc87291@inetpsa.com/airflow-repo"
+    git_url = "https://github.com/lazkamal/dags_2.git"
+    create_repo = DatabricksReposCreateOperator(task_id='create_repo', repo_path=repo_path, git_url=git_url, databricks_conn_id='databricks_default')
