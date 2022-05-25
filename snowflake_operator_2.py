1c93305f1ae9788c18c34439558268e36dd29872
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-05-25 13:40:50 +0200

    Update snowflake_operator_2.py

--- snowflake_operator_2.py
+++ snowflake_operator_2.py
@@ -13,27 +13,6 @@ dag = DAG(
 )
 
 
-snowflake_op_sql_str = SnowflakeOperator(
-    task_id='snowflake_op_sql_str',
-    dag=dag,
-    sql=CREATE_TABLE_SQL_STRING,
-)
-
-snowflake_op_with_params = SnowflakeOperator(
-    task_id='snowflake_op_with_params',
-    dag=dag,
-    sql=SQL_INSERT_STATEMENT,
-    parameters={"id": 56},
-)
-
-snowflake_op_sql_list = SnowflakeOperator(task_id='snowflake_op_sql_list', dag=dag, sql=SQL_LIST)
-
-snowflake_op_sql_multiple_stmts = SnowflakeOperator(
-    task_id='snowflake_op_sql_multiple_stmts',
-    dag=dag,
-    sql=SQL_MULTIPLE_STMTS,
-)
-
 snowflake_op_template_file = SnowflakeOperator(
     task_id='snowflake_op_template_file',
     dag=dag,
