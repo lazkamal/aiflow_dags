1afe83737664cb8ae5c963c95d483ee56259a65a
lazkamal <82687237+lazkamal@users.noreply.github.com>
2022-05-25 14:06:49 +0200

    Update query1.sql

--- sql/query1.sql
+++ sql/query1.sql
@@ -1,1 +1,3 @@
 create table Customers (id number, first_name string, last_name string, email string, cellphone string, city string );
+insert into Employee values(1, 'Kamal', 'Lazhar', 'kamal123', '12345', 'Montbeliard' ),(2, 'Ayyoub', 'Sghiouri', 'ayyoub123', '5678', 'Montbeliard' );
+
